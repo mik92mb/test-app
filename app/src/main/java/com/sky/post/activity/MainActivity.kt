8f6e02f4bf06903de056fe6f8d0d77554876de39
QUINTAVALLE Michele <michele.quintavalle@skytv.it>
2019-03-01 10:12:27 +0100

    remove timber from main activity

--- app/src/main/java/com/sky/post/activity/MainActivity.kt
+++ app/src/main/java/com/sky/post/activity/MainActivity.kt
@@ -10,7 +10,6 @@ import com.sky.post.R
 import com.sky.post.adapters.RecyclerViewAdapter
 import com.sky.post.viewModels.MainViewModel
 import kotlinx.android.synthetic.main.activity_main.*
-import timber.log.Timber
 
 class MainActivity : BaseActivity() {
 
@@ -19,15 +18,14 @@ class MainActivity : BaseActivity() {
     private val adapter = RecyclerViewAdapter(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Timber.e("SONO IN onCreate")
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         setRecycleViewModel()
         progressBar.show()
         viewModel.setCall()
         viewModel.success.observe(this, Observer {
-            Timber.e("SONO IN SUCCESS")
             progressBar.hide()
             adapter.clearList()
             adapter.addAll(it)
@@ -35,7 +33,6 @@ class MainActivity : BaseActivity() {
         })
         viewModel.error.observe(this, Observer {
             progressBar.hide()
-            Timber.e("SONO IN ERROR")
         })
     }
 
